6990a947114dd5a2139937a4f34764c8ea5e9e4c
Mohammed safvan <mohammedsafvannk@gmail.com>
2020-11-22 09:04:00 +0530

    minor changes

--- routes/admin.js
+++ routes/admin.js
@@ -1,50 +1,43 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var productHelper=require('../helpers/product-helpers')
-
-
+var productHelper = require("../helpers/product-helpers");
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get("/", function (req, res, next) {
   productHelper.getAllProducts().then((products) => {
-    res.render('admin/view-products', { admin: true, products })
-  })
-
+    res.render("admin/view-products", { admin: true, products });
+  });
 });
 
-router.get('/all-products', (req, res, next) => {
+router.get("/all-products", (req, res, next) => {
   productHelper.getAllProducts().then((products) => {
-    res.render('admin/all-products', { admin: true, products })
-  })
-})
+    res.render("admin/all-products", { admin: true, products });
+  });
+});
 
-router.get('/add-product', (req, res) => {
-  res.render('admin/add-products')
-})
+router.get("/add-product", (req, res) => {
+  res.render("admin/add-products", { admin: true });
+});
 
-router.post('/add-product', (req, res) => {
+router.post("/add-product", (req, res) => {
   productHelper.addProduct(req.body, (id) => {
-    let image = req.files.image
-    image.mv('./public/product-images/' + id + '.jpg', (err, done) => {
+    let image = req.files.image;
+    image.mv("./public/product-images/" + id + ".jpg", (err, done) => {
       if (!err) {
-        res.render('admin/add-products')
+        res.render("admin/add-products");
       } else {
-        console.log(err)
+        console.log(err);
       }
-    })
-    
-  })
-})
+    });
+  });
+});
 
-router.get('/all-orders', (req, res) => {
-  res.send('all orders')
-})
+router.get("/all-orders", (req, res) => {
+  res.send("all orders");
+});
 
-router.get('/all-users', (req, res) => {
-  res.send('all users')
-})
-
-
-
+router.get("/all-users", (req, res) => {
+  res.send("all users");
+});
 
 module.exports = router;
